133fdc14de1e66f50005d022033e84af9134c1e2
stef_fab@hotmail.it <stef_fab@hotmail.it>
2022-01-10 14:29:05 +0100

    add 1

---
+++ classes/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- Oggi pomeriggio provate ad immaginare alcune classi necessarie per creare uno shop online; ad esempio, ci saranno sicuramente dei prodotti da acquistare e degli utenti che fanno shopping.
+Strutturare le classi gestendo l’ereditarietà dove necessario; ad esempio ci potrebbero essere degli utenti premium che hanno diritto a degli sconti esclusivi, oppure diverse tipologie di prodotti.
+Eseguire poi degli output istanziando oggetti delle varie classi. -->
+
+    <h1>Esercizo shop 02</h1>
+</body>
+
+</html>
